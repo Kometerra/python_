abf2005c6519fd28073311ac73628aaba49ceb1c
kometerra <kometerra@gmail.com>
2025-09-12 17:14:29 +0300

    API test

---
+++ Helpers/Assertions.py
@@ -0,0 +1,24 @@
+from requests import Response
+
+class Asserts:
+
+    def assert_status_code(response: Response, expected_code: int):
+        actual_code = response.status_code
+        assert actual_code == expected_code, \
+        f'Ожидался статус код "{expected_code}", но получен {actual_code}. Тело ответа {response.text}'
+
+    def assert_json_has_key(response: Response, key: str):
+        response_json = response.json()
+        assert key in response_json, \
+        f'Ключ "{key}" отсутствует в JSON - ответе'
+
+    def assert_json_is_list(response: Response):
+        response_json = response.json()
+        assert isinstance(response_json,list), \
+        f'Ожидался тип данных list'
+
+    def assert_json_has_key_value(response: Response, key: str, value):
+        response_json = response.json()
+        assert key in response_json, f'Ключ "{key}" отсутствует в JSON - ответе'
+        actual_value = response_json[key]
+        assert actual_value == value, f'Ожидалось "{value}" \n получено "{actual_value}"'
---
+++ Tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+import logging
+from dotenv import load_dotenv
+from api_frame.todo_api import TodoApi
+
+load_dotenv()
+
+# 🔧 Настройка логирования один раз при запуске Pytest
+def pytest_configure(config):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.StreamHandler(),  # вывод в консоль
+            logging.FileHandler("test.log")  # запись в файл
+        ]
+    )
+    logging.info("Логирование инициализировано через conftest.py")
+
+# 🌐 Фикстура для base_url
+@pytest.fixture(scope="session")
+def get_base_url():
+    return os.getenv('BASE_URL')
+
+# 📦 Фикстура для API-клиента
+@pytest.fixture()
+def get_todo_data(get_base_url):
+    return TodoApi(get_base_url)
