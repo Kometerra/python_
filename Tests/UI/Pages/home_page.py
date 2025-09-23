3553b0617958aa227f72b6951a775ff4e71ca146
kometerra <kometerra@gmail.com>
2025-09-23 19:40:12 +0300

    UI тест + API правки

--- Tests/UI/Pages/home_page.py
+++ Tests/UI/Pages/home_page.py
@@ -3,7 +3,7 @@ from Tests.UI.Pages.base_page import BasePage
 class HomePage(BasePage):
     JSON_SERVER_BUTTON_XPATH = "//a[text()='{text}']"
     ENDPOINT='/'
-    MENU_CREDIT_BUTTON = "a[data-label='Кредиты']"
+    MENU_CREDIT_BUTTON = "a[data-label='{text}']"
 
     def open(self):
         self.open_page(self.ENDPOINT)
@@ -16,6 +16,10 @@ class HomePage(BasePage):
         xpath = self.JSON_SERVER_BUTTON_XPATH.format(text=text)
         return self.find(xpath)
 
-    def get_menu_credit_button(self):
-        css = self.MENU_CREDIT_BUTTON
+    def get_menu_credit_button(self,text:str):
+        css = self.MENU_CREDIT_BUTTON.format(text=text)
         return self.find(css)
+
+    def click_menu_button(self, text: str):
+        css = self.MENU_CREDIT_BUTTON.format(text=text)
+        self.click(css)
